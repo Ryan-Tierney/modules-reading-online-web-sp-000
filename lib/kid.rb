e62b8e4563e3392f7b6f34af2b4311956ed1afbb
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-02 17:41:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
     class Kid
-      extend FancyDance::ClassMethods
+      extend Dancer::FanceyDance
       include FancyDance::InstanceMethods
     end
